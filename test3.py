d1fe38c4656bacb620df548f402c990ad87e99b6
e <szyyyds@qq.com>
2022-08-20 17:07:30 +0800

    Merge branch 'master' of E:\practise\Teng with conflicts.

--- test3.py
+++ test3.py
@@ -1,9 +1,14 @@
+ls = 'eq54325trrtertebrw'
+print(len(ls))
+num = 0
+for i in ls:
+    num = num+1
+print(num)
 
-def res():
-    inde()
+print('dsasda', end='\t')
 
 def inde():
-    print('1')
+    print('这是一个函数')
     return '312'
-
-res()
+res = inde()
+print(res)
